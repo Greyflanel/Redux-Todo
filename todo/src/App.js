063231043cb742b99c3c1c88518f44389d82c18d
Greyflanel <richardlovelace811@gmail.com>
2019-05-08 18:23:08 -0500

    Still working on it.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,13 +1,35 @@
 import React from 'react';
+import { Component } from 'react'
 import './App.css';
-import Todos from './container/Todos'
+import Todos from './container/Todos';
 
-function App() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      newTodo: ""
+    };
+  
+  // this.addTodo = this.addTodo.bind(this);
+    };
+
+addTodo(event) {
+  event.preventDefault();
+  this.props.addTodo({
+    value: this.state.newTodo,
+    complete: false
+  });
+  this.setState({
+    newTodo: ""
+  });
+};
+
+  render() {
   return (
     <div className="App">
        <Todos />
     </div>
   );
 }
-
+}
 export default App;
--- todo/src/container/Todos.js
+++ todo/src/container/Todos.js
@@ -1,27 +1,27 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class Todo extends Component {
-    
-    render() {
-        return (
-<div>
-<h1>Todo List</h1>
-<ul>
-    <li>Wash dishes</li>
-</ul>
-<input type="text"/>
-<button>Add Todos</button>
-</div>
-
-        )
-    }
+  render() {
+      console.log(this.state);
+      
+    return (
+      <div>
+        <h1>Todo List</h1>
+        <ul>
+          <li>Wash dishes</li>
+        </ul>
+        <input type="text" />
+        <button>Add Todos</button>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        todos: state.todos
-    };
+const mapStateToProps = state => {
+  return {
+    todos: state.todos
+  };
 };
 
 export default connect(mapStateToProps)(Todo);
