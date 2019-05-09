58ea4825fb8e6efd4e8616ce30d3ff4202aca73d
Greyflanel <richardlovelace811@gmail.com>
2019-05-09 12:52:46 -0500

    New changes

--- todo/src/reducers/reducer.js
+++ todo/src/reducers/reducer.js
@@ -2,7 +2,7 @@ import { ADDTODO, DELETE_TODO } from '../actions/action.js';
 
 const initialState = [
      {
-        id: 0,
+         id: 0,
         value: "Mow the lawn",
         completed: false
     }
@@ -12,9 +12,14 @@ export default (state = initialState, action) => {
 
     switch (action.type) {
         case ADDTODO:
-        return Object.assign([], state).concat()
+        return Object.assign([], state).concat({
+            value: action.payload.value,
+            completed: action.payload.completed
+        })
         case DELETE_TODO:
-        return Object.assign([], state)
+        return Object.assign([], state).filter((todo, index) => {
+            return todo.id !== index;
+        })
     default:
     return state;
     }
